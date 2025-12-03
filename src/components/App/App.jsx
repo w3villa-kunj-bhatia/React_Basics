9b2bfec74829e88c8b59bba8ed4525cdbc243c2f
Kunj Bhatia <kunj.bhatia@w3villa.com>
2025-12-03 14:57:50 +0530

    refactor: restructure project by removing unused files and adding component structure

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import './App.css'
-import Hello from './Hello';
+import { useState } from "react";
+import "@/components/App/App.css";
+import Hello from "@/components/Hello/Hello";
 
 function App() {
   const username = "Kunj";
-    
+
   return (
     <div>
       <h1>Hello React!</h1>
